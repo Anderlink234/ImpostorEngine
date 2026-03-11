ab6704622ce09f764e913a2583be65d347447123
Anderson Davila H <Anderlink234@gmail.com>
2026-03-10 20:24:21 -0500

    Initial commit

---
+++ src/main/java/com/develop/ImpostorEngine/ImpostorEngineApplication.java
@@ -0,0 +1,13 @@
+package com.develop.ImpostorEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostorEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostorEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/ImpostorEngine/ImpostorEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.ImpostorEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostorEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
